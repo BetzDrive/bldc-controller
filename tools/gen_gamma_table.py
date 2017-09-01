d5a327b950d8595852438d4d5f6953a8b57eb774
allanzhao <allanzhao@users.noreply.github.com>
2017-08-31 18:48:21 -0700

    Added some useful Python scripts.
      - comms.py: Python library for communicating with the motor controller boards
      - upload_firmware.py: uploads firmware to the boards
      - gen_gamma_table.py: generates the lookup table for LED gamma correction

---
+++ tools/gen_gamma_table.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import sys
+
+NUM_STEPS = 2 ** 8
+MAX_VALUE = 52500
+VALUES_PER_LINE = 8
+GAMMA = 2.8
+
+sys.stdout.write('const uint16_t led_gamma_table[] = {')
+
+for i in range(NUM_STEPS):
+	if i % VALUES_PER_LINE == 0:
+		sys.stdout.write('\r\n  ')
+	value = int(round((float(i) / (NUM_STEPS - 1)) ** GAMMA * MAX_VALUE))
+	sys.stdout.write('{:5d}, '.format(value))
+
+sys.stdout.write('\r\n};\r\n')
---
+++ tools/upload_firmware.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import argparse
+from comms import *
+import serial
+import time
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Upload firmware to a motor controller board.')
+    parser.add_argument('serial', type=str, help='Serial port')
+    parser.add_argument('--baud_rate', type=int, help='Serial baud rate')
+    parser.add_argument('board_id', type=int, help='Board ID')
+    parser.add_argument('bin_file', type=str, help='.bin file containing firmware image')
+    parser.add_argument('--offset', type=int, help='Offset address for firmware image')
+    parser.set_defaults(baud_rate=COMM_DEFAULT_BAUD_RATE, offset=COMM_DEFAULT_FIRMWARE_OFFSET)
+    args = parser.parse_args()
+
+    ser = serial.Serial(port=args.serial, baudrate=args.baud_rate, timeout=0.1)
+    time.sleep(0.1)
+    ser.reset_input_buffer()
+
+    client = BLDCControllerClient(ser)
+
+    flash_sector_map = client.getFlashSectorMap(args.board_id)
+
+    with open(args.bin_file, 'rb') as bin_file:
+        firmware_image = bin_file.read()
+
+    success = client.writeFlash(args.board_id, args.offset, firmware_image, sector_map=flash_sector_map, print_progress=True)
+
+    if success:
+        client.leaveBootloader(args.board_id)
+        print "Success"
+    else:
+        print "Failed"
+
+    ser.close()
