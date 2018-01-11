f8d7804cf54748c9a0609c08dddf23aba5a9e998
allanzhao <allanzhao@users.noreply.github.com>
2018-01-10 17:22:18 -0800

    Implemented software endstops

--- tools/duty_cycle.py
+++ tools/duty_cycle.py
@@ -28,13 +28,25 @@ needs_flip_phase = [2, 3, 13, 17, 22, 23]
 has_21_erevs_per_mrev = [18, 19, 20, 21]
 
 client.writeRegisters(address, 0x0101, 1, struct.pack('<H', angle_mapping[address]) )
-client.writeRegisters(address, 0x0106, 1, struct.pack('<f', duty_cycle) )
 client.writeRegisters(address, 0x0102, 1, struct.pack('<B', 0) )
 client.writeRegisters(address, 0x0109, 1, struct.pack('<B', int(address in needs_flip_phase)) )
 try:
     client.writeRegisters(address, 0x010a, 1, struct.pack('<B', 21 if (address in has_21_erevs_per_mrev) else 14))
 except:
     print "WARNING: Motor driver board does not support erevs_per_mrev, try updating the firmware."
+
+start_angle = struct.unpack('<f', client.readRegisters(address, 0x010b, 1))[0]
+client.writeRegisters(address, 0x0110, 1, struct.pack('<f', start_angle - 0.5))
+client.writeRegisters(address, 0x0111, 1, struct.pack('<f', start_angle))
+client.writeRegisters(address, 0x0112, 1, struct.pack('<f', 20.0))
+
+client.writeRegisters(address, 0x0106, 1, struct.pack('<f', duty_cycle))
+
+# while True:
+#     client.writeRegisters(address, 0x0106, 1, struct.pack('<f', duty_cycle))
+#     time.sleep(5)
+#     client.writeRegisters(address, 0x0106, 1, struct.pack('<f', -duty_cycle))
+#     time.sleep(5)
 
 # while True:
 #     try:
