0427cc5119c3e04c83163368c79a6afc6b9f0d26
allanzhao <allanzhao@users.noreply.github.com>
2018-02-07 19:05:15 -0800

    Reformatting and refactoring recorder code
      - moved constants to constants.h
      - limited scope of recorder state enum (no longer global)
      - fw_comms.cpp: moved temp variables into corresponding switch cases

--- firmware/include/Recorder.h
+++ firmware/include/Recorder.h
@@ -1,32 +1,35 @@
 #ifndef _RECORDER_H_
 #define _RECORDER_H_
+
 #include <stddef.h>
 #include "constants.h"
 
-constexpr size_t NUM_SAMPLES = 1000;
-
-enum RecordState {
-  READY,
-  RECORDING,
-  FINISHED
-};
-
 namespace motor_driver {
-
 
 class Recorder {
 public:
-  Recorder(): cur_state_(READY), index_(0) {}
+  Recorder() : cur_state_(State::READY), index_(0) {}
+
   bool startRecord();
+
   void recordSample(float* recorder_new_data);
+
   float* read();
+
   void reset();
+
   uint16_t size();
 
 private:
-  RecordState cur_state_;
+  enum class State {
+    READY,
+    RECORDING,
+    FINISHED
+  };
+
+  State cur_state_;
   size_t index_;
-  float record_buf_[NUM_SAMPLES * recorder_channel_count];
+  float record_buf_[recorder_max_samples * recorder_channel_count];
 };
 
 } // namespace motor_driver
--- firmware/src/Recorder.cpp
+++ firmware/src/Recorder.cpp
@@ -4,27 +4,27 @@ namespace motor_driver {
 
 bool Recorder::startRecord() {
   // TODO: start another record as soon as it is over, if already recording
-  if (cur_state_ == READY) {
-    cur_state_ = RECORDING;
+  if (cur_state_ == State::READY) {
+    cur_state_ = State::RECORDING;
     return true;
   }
   return false;
 }
 
 void Recorder::recordSample(float *recorder_new_data) {
-  if (cur_state_ == RECORDING) {
+  if (cur_state_ == State::RECORDING) {
     for (size_t j = 0; j < recorder_channel_count; j++) {
       record_buf_[index_ + j] = recorder_new_data[j];
     }
     index_ += recorder_channel_count;
-    if (index_ >= NUM_SAMPLES * recorder_channel_count) {
-      cur_state_ = FINISHED;
+    if (index_ >= recorder_max_samples * recorder_channel_count) {
+      cur_state_ = State::FINISHED;
     }
   }
 }
 
-float* Recorder::read() {
-  if (cur_state_ == FINISHED) {
+float *Recorder::read() {
+  if (cur_state_ == State::FINISHED) {
     return record_buf_;
   } else {
     return nullptr;
@@ -33,14 +33,15 @@ float* Recorder::read() {
 
 void Recorder::reset() {
   index_ = 0;
-  cur_state_ = READY;
+  cur_state_ = State::READY;
 }
 
 uint16_t Recorder::size() {
-  if (cur_state_ == FINISHED) {
-    return NUM_SAMPLES * recorder_channel_count;
+  if (cur_state_ == State::FINISHED) {
+    return recorder_max_samples * recorder_channel_count;
+  } else {
+    return 0;
   }
-  return 0;
 }
 
 } // namespace motor_driver
