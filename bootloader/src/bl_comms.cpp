938114a4baf5c5ddfd3b7e5bcdce6202d7c9dc40
allanzhao <allanzhao@users.noreply.github.com>
2018-06-08 17:18:54 -0700

    Reduce size of bootloader

--- bootloader/src/bl_comms.cpp
+++ bootloader/src/bl_comms.cpp
@@ -12,17 +12,8 @@ void commsRegAccessHandler(comm_addr_t start_addr, size_t reg_count, uint8_t *bu
 
   for (comm_addr_t addr = start_addr; addr < start_addr + reg_count; addr++) {
     switch (addr) {
-      case 5:
-        if (access_type == RegAccessType::WRITE) {
-          if (buf_len - index >= 3) {
-            setStatusLEDColor(buf[index], buf[index + 1], buf[index + 2]);
-            index += 3;
-          } else {
-            errors |= COMM_ERRORS_BUF_LEN_MISMATCH;
-            return;
-          }
-        }
-        break;
+      // No registers
+      
       default:
         errors |= COMM_ERRORS_INVALID_ARGS;
         return;
