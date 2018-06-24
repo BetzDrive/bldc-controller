42bbfd521bfe35d2fa2d7a55b9c544f1c5e24753
allanzhao <allanzhao@users.noreply.github.com>
2018-06-23 21:06:59 -0700

    Fixed duty cycle script
      - Don't apply EAC if it isn't available
      - "Electrical" -> "Encoder" angle compensation

--- tools/duty_cycle.py
+++ tools/duty_cycle.py
@@ -36,7 +36,7 @@ for address, duty_cycle in zip(addresses, duty_cycles):
         client.setERevsPerMRev(address, calibration_obj['epm'])
         client.setTorqueConstant(address, calibration_obj['torque'])
         client.setPositionOffset(address, calibration_obj['zero'])
-        if calibration_obj['eac_type'] == 'int8':
+        if 'eac_type' in calibration_obj and calibration_obj['eac_type'] == 'int8':
             print('EAC calibration available')
             try:
                 client.writeRegisters(address, 0x1100, 1, struct.pack('<f', calibration_obj['eac_scale']))
@@ -47,7 +47,7 @@ for address, duty_cycle in zip(addresses, duty_cycles):
                     table_slice = calibration_obj['eac_table'][i:i+slice_len]
                     client.writeRegisters(address, 0x1200+i, len(table_slice), struct.pack('<{}b'.format(len(table_slice)), *table_slice))
             except ProtocolError:
-                print('WARNING: Motor driver board does not support electrical angle compensation, try updating the firmware.')
+                print('WARNING: Motor driver board does not support encoder angle compensation, try updating the firmware.')
         client.setCurrentControlMode(address)
         client.writeRegisters(address, 0x1030, 1, struct.pack('<H', 1000))
         # print("Motor %d ready: supply voltage=%fV", address, client.getVoltage(address))
