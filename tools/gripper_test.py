03cc682c7e551c2a829c8d7034ca37dc1ce3ade0
allanzhao <allanzhao@users.noreply.github.com>
2018-07-06 17:27:45 -0700

    Updated gripper test script

--- tools/gripper_test.py
+++ tools/gripper_test.py
@@ -22,43 +22,43 @@ except ValueError:
     addresses = [int(address_str) for address_str in sys.argv[2].split(',')]
     duty_cycles = [float(duty_cycle_str) for duty_cycle_str in sys.argv[3].split(',')]
 
-client = BLDCControllerClient(s, protocol_v2=PROTOCOL_V2)
+client = BLDCControllerClient(s)
 
 for address, duty_cycle in zip(addresses, duty_cycles):
-    client.leaveBootloader(address)
+    client.leaveBootloader([address])
     time.sleep(0.2)
     s.reset_input_buffer()
 
     if PROTOCOL_V2:
-        calibration_obj = client.readCalibration(address)
+        calibration_obj = client.readCalibration([address])
 
-        client.setZeroAngle(address, calibration_obj['angle'])
-        client.setInvertPhases(address, calibration_obj['inv'])
-        client.setERevsPerMRev(address, calibration_obj['epm'])
-        client.setTorqueConstant(address, calibration_obj['torque'])
-        client.setPositionOffset(address, calibration_obj['zero'])
+        client.setZeroAngle([address], [calibration_obj['angle']])
+        client.setInvertPhases([address], [calibration_obj['inv']])
+        client.setERevsPerMRev([address], [calibration_obj['epm']])
+        client.setTorqueConstant([address], [calibration_obj['torque']])
+        client.setPositionOffset([address], [calibration_obj['zero']])
         if 'eac_type' in calibration_obj and calibration_obj['eac_type'] == 'int8':
             print('EAC calibration available')
             try:
-                client.writeRegisters(address, 0x1100, 1, struct.pack('<f', calibration_obj['eac_scale']))
-                client.writeRegisters(address, 0x1101, 1, struct.pack('<f', calibration_obj['eac_offset']))
+                client.writeRegisters([address], [0x1100], [1], [struct.pack('<f', calibration_obj['eac_scale'])])
+                client.writeRegisters([address], [0x1101], [1], [struct.pack('<f', calibration_obj['eac_offset'])])
                 eac_table_len = len(calibration_obj['eac_table'])
                 slice_len = 64
                 for i in range(0, eac_table_len, slice_len):
                     table_slice = calibration_obj['eac_table'][i:i+slice_len]
-                    client.writeRegisters(address, 0x1200+i, len(table_slice), struct.pack('<{}b'.format(len(table_slice)), *table_slice))
+                    client.writeRegisters([address], [0x1200+i], [len(table_slice)], [struct.pack('<{}b'.format(len(table_slice)), *table_slice)])
             except ProtocolError:
                 print('WARNING: Motor driver board does not support encoder angle compensation, try updating the firmware.')
-        client.setCurrentControlMode(address)
-        client.writeRegisters(address, 0x1030, 1, struct.pack('<H', 1000))
-        client.writeRegisters(address, 0x2000, 1, struct.pack('<B', 2) ) # Torque control
+        client.setCurrentControlMode([address])
+        client.writeRegisters([address], [0x1030], [1], [struct.pack('<H', 1000)])
+        client.writeRegisters([address], [0x2000], [1], [struct.pack('<B', 2)]) # Torque control
         # print("Motor %d ready: supply voltage=%fV", address, client.getVoltage(address))
 
 num_grips = 0
-start_pos = struct.unpack('<f', client.readRegisters(address, 0x3000, 1))[0]
+start_pos = struct.unpack('<f', client.readRegisters([address], [0x3000], [1])[0])[0]
 overheated = False
 
-while True:
+while num_grips < 1000:
     now = datetime.datetime.now()
     # Shutoff between 11PM and 10AM
     if now.hour >= 10 and now.hour < 23:
@@ -71,8 +71,8 @@ while True:
         last_time = time.time()
         while (time.time() - last_time < 1):
             try:
-                client.writeRegisters(address, 0x2006, 1, struct.pack('<f', duty_cycle))
-                state = struct.unpack('<ffffff', client.readRegisters(address, 0x3000, 6))
+                client.writeRegisters([address], [0x2006], [1], [struct.pack('<f', duty_cycle)])
+                state = struct.unpack('<ffffff', client.readRegisters([address], [0x3000], [6])[0])
             except Exception as e:
                 print(str(e))
                 pass
@@ -82,8 +82,8 @@ while True:
         # Open the gripper until position is 0
         while (state[0] > start_pos):
             try:
-                client.writeRegisters(address, 0x2006, 1, struct.pack('<f', -duty_cycle))
-                state = struct.unpack('<ffffff', client.readRegisters(address, 0x3000, 6))
+                client.writeRegisters([address], [0x2006], [1], [struct.pack('<f', -duty_cycle)])
+                state = struct.unpack('<ffffff', client.readRegisters([address], [0x3000], [6])[0])
             except Exception as e:
                 print(str(e))
                 pass
@@ -100,6 +100,7 @@ while True:
             myfile.write(str(time.time()) + ", " + str(closed_pos) + ", " + str(opened_pos) + ", " + str(temperature) + "\n")
             myfile.close()
 
+        print (num_grips)
         print (str(time.time()) + ", " + str(closed_pos) + ", " + str(opened_pos) + ", " + str(temperature) + "\n")
-
+        num_grips += 1
         
