f8644f49966163570171060b7ad0bdc4b4a76321
Brent Yi <yibrenth@gmail.com>
2019-05-18 02:38:35 -0700

    Revert "ISNS bugfixing and debugging enhancements"

--- common/include/constants.h
+++ common/include/constants.h
@@ -24,10 +24,9 @@ constexpr float rad_per_enc_tick = -2.0f * pi / encoder_period;
 
 constexpr unsigned int motor_pwm_clock_freq = 168000000; // Hz
 
-constexpr unsigned int motor_pwm_cycle_freq = 20000; // Hz
+constexpr unsigned int motor_pwm_cycle_freq = 40000; // Hz
 
-// TIM3 can't handle the full 168MHz like TIM1
-constexpr unsigned int adc_pwm_cycle_freq = motor_pwm_clock_freq/2; // Hz
+constexpr unsigned int adc_pwm_cycle_freq = motor_pwm_clock_freq / 2; // Hz
 
 constexpr float current_control_freq = motor_pwm_cycle_freq / 2.0f; // Current control runs every two PWM cycles
 constexpr float current_control_interval = 1.0f / current_control_freq;
@@ -42,31 +41,26 @@ constexpr size_t ivsense_sample_buf_depth = ivsense_samples_per_cycle * 2; // "d
 
 constexpr size_t ivsense_channel_count = 7; // 4 voltage channels, 3 current channels
 
-constexpr size_t ivsense_channel_ia = 0;    // Phase A current channel index
-constexpr size_t ivsense_channel_ib = 1;    // Phase B current channel index
-constexpr size_t ivsense_channel_ic = 2;    // Phase C current channel index
-constexpr size_t ivsense_channel_va = 3;    // Phase A voltage channel index
-constexpr size_t ivsense_channel_vb = 4;    // Phase B voltage channel index
-constexpr size_t ivsense_channel_vc = 5;    // Phase C voltage channel index
+constexpr size_t ivsense_channel_vc = 0;    // Phase A current channel index
+constexpr size_t ivsense_channel_vb = 1;    // Phase B current channel index
+constexpr size_t ivsense_channel_va = 2;    // Phase C current channel index
+constexpr size_t ivsense_channel_ia = 3;    // Phase A voltage channel index
+constexpr size_t ivsense_channel_ib = 4;    // Phase B voltage channel index
+constexpr size_t ivsense_channel_ic = 5;    // Phase C voltage channel index
 constexpr size_t ivsense_channel_vin = 6;   // Supply voltage channel index
 
 constexpr float ivsense_voltage_ratio = (2.21e3f + 39.2e3f) / 2.21e3f;      // Ratio of actual voltage to ADC input voltage
 constexpr float ivsense_current_shunt_value = 0.01f;                        // Current shunt resistor value, ohms
-constexpr float ivsense_current_amp_input_divider_gain = 0.97209f;          // Ratio of the input divider to the isns amp
-constexpr float ivsense_current_amp_gain = 43.1f;                           // Current shunt amplifier gain, V/V
-constexpr float ivsense_current_amp_composite_gain = ivsense_current_amp_input_divider_gain*ivsense_current_amp_gain;
-constexpr float ivsense_current_amp_offset = 0.03366f;                      // Voltage offset pre-gain (to handle negative currents)
+constexpr float ivsense_current_amp_gain = 41.2f;                           // Current shunt amplifier gain, V/V
+constexpr float ivsense_current_amp_offset = 0.02f;                         // Voltage offset pre-gain (to handle negative currents)
 constexpr float adc_vref_voltage = 3.3f;                                    // ADC reference voltage, volts
 constexpr unsigned int adc_max_value = 1u << 12;                            // ADC maximum value
-constexpr float adc_v_per_count = adc_vref_voltage / adc_max_value;
-
-constexpr float max_duty_cycle (0.9f); // Maximum allowable duty cycle before gate on-time interferes with ADC sampling
 
 /* Maximum expected voltage measurement */
 constexpr float ivsense_voltage_max = adc_vref_voltage * ivsense_voltage_ratio;
 
 /* Maximum expected current measurement */
-constexpr float ivsense_current_max = (1.0f / ivsense_current_shunt_value) * ((1.0f / ivsense_current_amp_composite_gain) * adc_vref_voltage - ivsense_current_amp_offset);
+constexpr float ivsense_current_max = (1.0f / ivsense_current_shunt_value) * ((1.0f / ivsense_current_amp_gain) * adc_vref_voltage - ivsense_current_amp_offset);
 
 /* Current at zero volts (also the minimum) */
 constexpr float ivsense_current_zero_voltage = (1.0f / ivsense_current_shunt_value) * (-ivsense_current_amp_offset);
@@ -78,9 +72,7 @@ constexpr float ivsense_voltage_zero_current = ivsense_current_amp_gain * (ivsen
 constexpr float ivsense_voltage_per_count = ivsense_voltage_max / adc_max_value;
 
 /* Actual current per ADC count */
-// BUG HERE! This formula assumes the reference current is exactly halfway between the rails, which it isnt'.
-//constexpr float ivsense_current_per_count = (ivsense_current_max - ivsense_current_zero_voltage) / adc_max_value;
-constexpr float ivsense_current_per_count = adc_v_per_count / (ivsense_current_amp_composite_gain*ivsense_current_shunt_value);
+constexpr float ivsense_current_per_count = (ivsense_current_max - ivsense_current_zero_voltage) / adc_max_value;
 
 /* ADC Value zero current is centered on */
 constexpr float ivsense_count_zero_current = ivsense_voltage_zero_current / adc_vref_voltage * adc_max_value;
@@ -108,7 +100,6 @@ constexpr uint8_t control_mode_torque = 2;
 constexpr uint8_t control_mode_velocity = 3;
 constexpr uint8_t control_mode_position = 4;
 constexpr uint8_t control_mode_position_velocity = 5;
-constexpr uint8_t control_mode_fixed_ang_vel = 6;
 
 /* Encoder modes */
 constexpr uint8_t encoder_mode_none = 0;
--- firmware/openocd_pipe.sh
+++
@@ -1,6 +0,0 @@
-# This file is not meant to be run directly by the user, it is called by gdb
-set -m
-trap '' INT
-
-openocd -c "gdb_port pipe" -f "openocd_gdb.cfg" -l /dev/null
-
