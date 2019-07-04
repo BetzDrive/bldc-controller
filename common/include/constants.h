a48f5c4dca6257df05222f57bf66d9e53942b9f5
Greg Balke <gbalke@berkeley.edu>
2019-07-03 19:36:12 -0700

    Adjusting current control back up to 20kHz (system can operate fully at this speed).

--- common/include/constants.h
+++ common/include/constants.h
@@ -29,7 +29,8 @@ constexpr unsigned int motor_pwm_cycle_freq = 20000; // Hz
 // TIM3 can't handle the full 168MHz like TIM1
 constexpr unsigned int adc_pwm_cycle_freq = motor_pwm_clock_freq/2; // Hz
 
-constexpr float current_control_freq = 1000; // Current control runs every PWM cycle
+// Current control should be a factor of the motor_pwm_cycle_freq
+constexpr float current_control_freq = motor_pwm_cycle_freq;  
 constexpr float current_control_interval = 1.0f / current_control_freq;
 constexpr float velocity_control_interval = current_control_interval;
 constexpr float position_control_interval = current_control_interval;
@@ -80,8 +81,6 @@ constexpr float ivsense_voltage_zero_current = ivsense_current_amp_gain * (ivsen
 constexpr float ivsense_voltage_per_count = ivsense_voltage_max / adc_max_value;
 
 /* Actual current per ADC count */
-// BUG HERE! This formula assumes the reference current is exactly halfway between the rails, which it isnt'.
-//constexpr float ivsense_current_per_count = (ivsense_current_max - ivsense_current_zero_voltage) / adc_max_value;
 constexpr float ivsense_current_per_count = adc_v_per_count / (ivsense_current_amp_composite_gain*ivsense_current_shunt_value);
 
 /* ADC Value zero current is centered on */
