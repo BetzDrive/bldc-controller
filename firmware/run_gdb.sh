597b4d9ff326167fe6eda6ec0d941742fb80e0f9
Greg Balke <gbalke@berkeley.edu>
2019-11-11 12:43:07 -0800

    GDB Multiarch replaced specific gdb versions

--- firmware/run_gdb.sh
+++ firmware/run_gdb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 GDB_LINK="https://developer.arm.com/-/media/Files/downloads/gnu-rm/8-2018q4/gcc-arm-none-eabi-8-2018-q4-major-linux.tar.bz2?revision=d830f9dd-cd4f-406d-8672-cca9210dd220?product=GNU%20Arm%20Embedded%20Toolchain,64-bit,,Linux,8-2018-q4-major"
-GDB=arm-none-eabi-gdb
+GDB=gdb-multiarch
 GCC=arm-none-eabi-gcc
 
 if ! [ -x "$(command -v $GDB)" ] ; then
