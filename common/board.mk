2af8b9fc44ef29688556a8dd428b9b9226d72716
Greg Balke <gbalke@berkeley.edu>
2020-07-23 23:21:22 -0700

    Better names/makefiles

--- common/board.mk
+++
@@ -1,9 +0,0 @@
-# List of all the board related files.
-BOARDSRC = ../chibios/boards/BLDC_SERVO_CONTROLLER/board.c
-
-# Required include directories
-BOARDINC = ../chibios/boards/BLDC_SERVO_CONTROLLER
-
-# Shared variables
-ALLCSRC += $(BOARDSRC)
-ALLINC  += $(BOARDINC)
