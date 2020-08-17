99e4e71d5bdc0c7c7901d886aa7709c66db8b718
Greg Balke <11024792+gbalke@users.noreply.github.com>
2020-08-16 19:42:59 -0700

    Renaming (#28)
    * Better names/makefiles
    
    * Updating documentation
    
    * Use common make imports for bootloader

---
+++ common/common.mk
@@ -0,0 +1,8 @@
+# ChibiOS libraries to include
+CHIBIOS = ../chibios
+include $(CHIBIOS)/os/hal/platforms/STM32F4xx/platform.mk
+include $(CHIBIOS)/os/hal/hal.mk
+include $(CHIBIOS)/os/ports/GCC/ARMCMx/STM32F4xx/port.mk
+include $(CHIBIOS)/os/kernel/kernel.mk
+include $(CHIBIOS)/test/test.mk
+
