6f4f3ba3c8f425c944555eb4af4b8881be598738
Greg Balke <gbalke@berkeley.edu>
2021-04-11 00:33:40 -0700

    Loop function broken up a bit more

--- tools/utils.py
+++ tools/utils.py
@@ -2,15 +2,12 @@
 import time
 from typing import Callable
 
+
 class DebugLoop:
-
     """Loops a function and prints statistics."""
-
-    def __init__(
-            self,
-            callback: Callable[[], bool],
-            iters_per_print: int = 1000
-    ):
+    def __init__(self,
+                 callback: Callable[[], bool],
+                 iters_per_print: int = 1000):
         """
         Initializes the loop variables.
 
@@ -23,27 +20,36 @@ class DebugLoop:
         self._callback = callback
         self._iters_per_print = iters_per_print
 
+        self._errors = 0
+        self._iters = 0
+        self._iter_start_time = None
+
+    def _loop_func(self):
+        self._errors += 0 if self._callback() else 1
+        self._iters += 1
+
+        if iters % self._iters_per_print == 0:
+            self._print_func()
+
+    def _print_func(self):
+        now = time.time()
+        diff = (now - self._start_time)
+
+        freq = self._iters_per_print / diff
+        error_rate = self._errors / self._iters_per_print * 100
+        print(f'Loop frequency is {round(freq, 2)}hz at '
+              f'{round(error_rate, 2)}% error rate.')
+
+        # Reset statistics variables.
+        self._start_time = now
+        self._errors = 0
+
     def loop(self):
         """Loops the callback. Can be escaped with ctrl^c."""
-        start_time = time.time()
-        errors = 0
-        iters = 0
+        self._iter_start_time = time.time()
         try:
             while True:
-                errors += 0 if self._callback() else 1
-                iters += 1
-
-                if iters % self._iters_per_print == 0:
-                    now = time.time()
-                    diff = (now - start_time)
-                    start_time = now
-
-                    freq = self._iters_per_print / diff
-                    error_rate = errors / self._iters_per_print * 100
-                    print(f'Loop frequency is {round(freq, 2)}hz at '
-                          f'{round(error_rate, 2)}% error rate.')
-
-                    errors = 0
+                self._loop_func()
         except KeyboardInterrupt:
             print()  # Clear line immediately after the ctrl-c
             print(f"Interrupted. Loop exiting. Completed {iters} iterations.")
