dddc28621d6385942c769b4c53d988440fba8331
Greg Balke <gbalke@berkeley.edu>
2021-05-16 19:52:18 -0700

    Renaming to test for all tools
    Signed-off-by: Greg Balke <gbalke@berkeley.edu>

--- tests/tools/unit/test_tools.py
+++ tests/tools/unit/test_tools.py
@@ -5,18 +5,17 @@ import pytest
 from tools import comms, read_sensor
 
 
-@dataclasses.dataclass
-class Args:
-    """Args for this tool."""
-    serial: str = '/dev/ttyUSB0'
-    board_ids: str = '1'
-    sensor: str = 'temp'
-    num_iters: int = 1
-    baud_rate: int = comms.COMM_DEFAULT_BAUD_RATE
-
-
 def test_read_sensor(mocker):
     """Ensures read sensor runs when given valid arguments."""
+    @dataclasses.dataclass
+    class Args:
+        """Args for this tool."""
+        serial: str = '/dev/ttyUSB0'
+        board_ids: str = '1'
+        sensor: str = 'temp'
+        num_iters: int = 1
+        baud_rate: int = comms.COMM_DEFAULT_BAUD_RATE
+
     mocker.patch('tools.read_sensor.serial')
     mocker.patch('tools.comms.BLDCControllerClient.enumerateBoards',
                  lambda self, x: x)
